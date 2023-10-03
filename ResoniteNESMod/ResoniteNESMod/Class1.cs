d79d9ce47d05236c35717fd875781edcf8ca7545
ikubay <ikubaysan@gmail.com>
2023-10-03 08:55:46 -0700

    Got log messages to display in log

---
+++ ResoniteNESMod/ResoniteNESMod/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Reflection;
+
+namespace ResoniteNESMod
+{
+    public class ResoniteNESMod : ResoniteMod
+    {
+        public override string Author => "Ikubaysan";
+        public override string Name => "ResoniteNESMod";
+        public override string Version => "1.0.0";
+
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> enabled = new ModConfigurationKey<bool>("enabled", "Should the mod be enabled", () => true); //Optional config settings
+
+        private static ModConfiguration Config;//If you use config settings, this will be where you interface with them
+
+        public override void OnEngineInit()
+        {
+            Config = GetConfiguration(); //Get this mods' current ModConfiguration
+            Config.Save(true); //If you'd like to save the default config values to file
+            Harmony harmony = new Harmony("com.ikubaysan.ResoniteNESMod"); //typically a reverse domain name is used here (https://en.wikipedia.org/wiki/Reverse_domain_name_notation)
+            harmony.PatchAll(); // do whatever LibHarmony patching you need, this will patch all [HarmonyPatch()] instances
+
+            //Various log methods provided by the mod loader, below is an example of how they will look
+            //3:14:42 AM.069 ( -1 FPS)  [INFO] [ResoniteModLoader/ExampleMod] a regular log
+            Debug("a debug log from ResoniteNESMod!!!");
+            Msg("a regular log from ResoniteNESMod!!!");
+            Warn("a warn log from ResoniteNESMod!!!");
+            Error("an error log from ResoniteNESMod!!!");
+        }
+
+
+    }
+}
